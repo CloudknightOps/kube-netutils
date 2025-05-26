bd733197c40f242613f03f425462261c77c8ff27
Theo <uviekugbere.theo@gmail.com>
2025-05-27 00:29:21 +0100

    Intial commit

---
+++ trivy-install.sh
@@ -0,0 +1,10 @@
+# !/bin/bash 
+#Add the trivy-repo
+apt-get  update
+apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+
+#Update Repo and Install trivy
+apt-get update
+apt-get install trivy
